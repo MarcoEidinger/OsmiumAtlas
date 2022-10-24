cca25d965f527907f43f1efa1fb361b86989a93e
Marco Eidinger <eidingermarco@gmail.com>
2022-10-24 13:49:23 -0700

    fix: remove accidental "/" in twitter handle

--- Sources/Twitter/String+Extension.swift
+++ Sources/Twitter/String+Extension.swift
@@ -6,5 +6,13 @@ public extension String {
             .replacingOccurrences(of: "http://twitter.com/", with: "")
             .replacingOccurrences(of: "https://www.twitter.com/", with: "")
             .replacingOccurrences(of: "http://www.twitter.com/", with: "")
+            .deletingSuffix("/")
+    }
+}
+
+extension String {
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
     }
 }
